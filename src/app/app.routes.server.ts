d969c6812fba2e2ee7c6d3b448afb464e469b9b5
Fernando <ferchin911@hotmail.com>
2026-03-09 20:59:11 -0600

    fix para hacer el deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { appRoutes } from './app.routes'; // Asegúrate de que apunte a tus rutas
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'producto/:id',
+    renderMode: RenderMode.Server, // 🌟 Le decimos que renderice esta en vivo
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // 🌟 El resto del sitio sí lo pre-construye
   }
 ];
